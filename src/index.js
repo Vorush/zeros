c4fe7c8b81058ad1fa694513be2b8cf55a8f64bc
Pavel Vorushilo <nexus5best5@gmail.com>
2019-10-03 20:18:30 +0300

    feat: add solving

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function zeros(expression) {
-  // your solution
+  
+  let fact = expression.split('*');
+
+  let countTwos = 0;
+  let countFives = 0;
+
+  for(let i = 0; i < fact.length; i++){
+
+    let move = 2;
+    let num;
+
+
+    if (fact[i].includes('!!')) {
+      num = fact[i].slice(0, -2);
+    }else {
+      move = 1;
+      num = fact[i].slice(0, -1);
+    } 
+
+    while(num > 0){
+
+      let tmpFive = num;
+      let tmpTwo = num;
+
+      while(tmpFive % 5 == 0){
+
+        countFives++;
+        tmpFive /= 5;
+
+      }
+
+      while(tmpTwo % 2 == 0){
+
+        countTwos++;
+        tmpTwo /= 2;
+        
+      }
+
+      num -= move;
+
+    }
+
+  }
+
+  return Math.min(countTwos, countFives); 
 }
